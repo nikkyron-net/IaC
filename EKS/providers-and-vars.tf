1f9f8b9fad8da6ed72f1c159e62f8d8f5f561dc7
Ron Fish <ron@upmyjob.com>
2023-03-13 13:19:02 -0500

    re add cloud backend to EKS

--- EKS/providers-and-vars.tf
+++ EKS/providers-and-vars.tf
@@ -42,13 +42,13 @@ terraform {
     }     
   }
   # Adding Backend as S3 for Remote State Storage
-  /*cloud {
-    organization = "upmyjob-com"
+  cloud {
+    organization = "nikkyron-net"
 
     workspaces {
       name = "EKS"
     }
-  } */
+  } 
 }
 
 # Terraform Provider Block
